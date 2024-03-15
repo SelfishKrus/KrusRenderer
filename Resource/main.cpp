5bc7c1fdad7badf9ee7ceddb24715b063c49511d
Krus_LEGION_4070 <selfishkrus@github.com>
2024-03-15 15:26:50 +0800

    optimization of drawing lines

--- Resource/main.cpp
+++ Resource/main.cpp
@@ -19,10 +19,17 @@ void line(int x0, int y0, int x1, int y1, TGAImage& image, TGAColor color)
         std::swap(y0, y1);
     }
 
+    int dx = x1 - x0;
+    int dy = y1 - y0;
+
+    // remap error by multiplying by 2 * dx 
+    int derror2 = std::abs(dy) * 2;
+    int error2 = 0;
+
+    int y = y0;
+
     for (int x = x0; x <= x1; x++) 
     {
-        float t = (x - x0) / (float)(x1 - x0);
-        int y = y0 * (1. - t) + y1 * t;
         if (steep) 
         {
             image.set(y, x, color); // if transposed, de−transpose 
@@ -31,17 +38,27 @@ void line(int x0, int y0, int x1, int y1, TGAImage& image, TGAColor color)
         {
             image.set(x, y, color);
         }
+
+        error2 += derror2;
+        if (error2 > dx)
+        {
+            y += (y1 > y0 ? 1 : -1);
+            error2 -= 2 * dx ;
+        }
     }
 }
 
 int main(int argc, char** argv) 
 {
     // initialize the image 
-    TGAImage image(100, 100, TGAImage::RGB);
+    TGAImage image(width, height, TGAImage::RGB);
 
     // draw 
     line(13, 20, 80, 40, image, white);
     line(80, 40, 13, 20, image, red);
+
+    line(30, 20, 18, 50, image, red);
+
 
     // write in 
     image.flip_vertically(); // i want to have the origin at the left bottom corner of the image
--- Resource/main.h
+++ Resource/main.h
@@ -4,3 +4,6 @@
 
 const TGAColor white = TGAColor(255, 255, 255, 255);
 const TGAColor red = TGAColor(255, 0, 0, 255);
+
+const int width = 256;
+const int height = 256;
