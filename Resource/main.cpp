d51ae7032855f1347e0b23717e914edea510f0d5
Krus_LEGION_4070 <selfishkrus@github.com>
2024-03-26 18:11:19 +0800

    rasterize a triangle with barycentric coordinates

--- Resource/main.cpp
+++ Resource/main.cpp
@@ -74,7 +74,8 @@ void DrawTriangleWireframe(Triangle triangle, TGAImage& image, TGAColor color)
     line(triangle.p2.x, triangle.p2.y, triangle.p0.x, triangle.p0.y, image, color);
 }
 
-void RasterizeTriangle(Triangle triangle, TGAImage& image, TGAColor color)
+// Line sweeping
+void RasterizeTriangle_LS(Triangle triangle, TGAImage& image, TGAColor color)
 {
     // sort the vertices p0, p1, p2 according to ascending y-coordinate
     if (triangle.p0.y > triangle.p1.y) std::swap(triangle.p0, triangle.p1);
@@ -114,6 +115,45 @@ void RasterizeTriangle(Triangle triangle, TGAImage& image, TGAColor color)
     }
 }
 
+Vec3f Barycentric(Triangle triangle, Vec2i P)
+{
+    Vec3f u = Vec3f(triangle.p2.x-triangle.p0.x, triangle.p1.x-triangle.p0.x, triangle.p0.x-P.x) ^ Vec3f(triangle.p2.y - triangle.p0.y, triangle.p1.y - triangle.p0.y, triangle.p0.y - P.y);
+    if (std::abs(u.z) < 1) return Vec3f(-1, 1, 1);
+    return Vec3f(1.0f-(u.x+u.y)/u.z, u.x/u.z, u.y/u.z);
+}
+
+
+// Barycentric Coordinate
+void RasterizeTriangle_BC(Triangle triangle, TGAImage& image, TGAColor color)
+{   
+    // Get bounding box of the triangle
+    Vec2i bboxMin = Vec2i(image.get_width() - 1, image.get_height() - 1);
+    Vec2i bboxMax = Vec2i(0, 0);
+    Vec2i clamp(image.get_width() - 1, image.get_height() - 1);
+
+    Vec2i vertices[3] = { triangle.p0, triangle.p1, triangle.p2 };
+    for (int i = 0; i < 3; i++) 
+    {
+        bboxMin.x = std::max(0, std::min(bboxMin.x, vertices[i].x));
+        bboxMin.y = std::max(0, std::min(bboxMin.y, vertices[i].y));
+        bboxMax.x = std::min(clamp.x, std::max(bboxMax.x, vertices[i].x));
+        bboxMax.y = std::min(clamp.y, std::max(bboxMax.y, vertices[i].y));
+    }
+
+    // Rasterize each pixel inside the bounding box 
+    // inside the triangle - fill 
+    // outside the triangle - blank
+    for (int i = bboxMin.x; i <= bboxMax.x; i++)
+    {
+        for (int j = bboxMin.y; j <= bboxMax.y; j++)
+        {
+            Vec3f bc_screen = Barycentric(triangle, Vec2i(i, j));
+            if (bc_screen.x < 0 || bc_screen.y < 0 || bc_screen.z < 0) continue;
+            image.set(i, j, color);
+        }
+    }
+}
+
 int main(int argc, char** argv) 
 {
     // initialize the image 
@@ -121,7 +161,7 @@ int main(int argc, char** argv)
 
     // draw 
     //DrawWireframe(model, image, white);
-    RasterizeTriangle(triangle, image, red);
+    RasterizeTriangle_BC(triangle, image, red);
     DrawTriangleWireframe(triangle, image, white);
 
     // write in 
